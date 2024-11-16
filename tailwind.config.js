6284a1153b9e5467737675548681c848eb219e3f
Anchuthan018 <147297524+Anchuthan018@users.noreply.github.com>
2024-11-16 12:52:33 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         'primary': "rgb(60, 168, 166)"
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },
